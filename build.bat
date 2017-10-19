fec5488ab51bffb7fb298da1fb8394b781982700
Arlen22 <arlenbee@gmail.com>
2017-10-19 08:49:56 -0400

    add call so it runs both commands

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-nexe -t windows-x64-6.11.4 src\server.js -o dist\server-win
-nexe -t mac-x64-6.11.4 src\server.js -o dist\server-mac
+
+call nexe -t windows-x64-6.11.4 src\server.js -o dist\server-win
+call nexe -t mac-x64-6.11.4 src\server.js -o dist\server-mac
