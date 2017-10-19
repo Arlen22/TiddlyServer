fc367a57b66ab7504881ad151e50529e9ee7d1ec
Arlen22 <arlenbee@gmail.com>
2017-10-19 08:14:45 -0400

    modify build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-nexe -t windows-x64-6.11.4 server.js -o dist/server
-nexe -t mac-x64-6.11.4 server.js -o dist/server
+nexe -t windows-x64-6.11.4 src\server.js -o dist\server-win
+nexe -t mac-x64-6.11.4 src\server.js -o dist\server-mac
