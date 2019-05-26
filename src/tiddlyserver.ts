6e0ebd7dec327b470ce2116bf788c5bb1b3a3440
arlen22 <arlenbee@gmail.com>
2019-05-26 22:15:29 +0800

    updates to tree

--- src/tiddlyserver.ts
+++ src/tiddlyserver.ts
@@ -4,7 +4,7 @@ import {
 	Directory, sortBySelector, obs_stat, obs_readdir, FolderEntryType, obsTruthy,
 	StatPathResult, DebugLogger, TreeObject, PathResolverResult, TreePathResult, resolvePath,
 	sendDirectoryIndex, statWalkPath, typeLookup, DirectoryIndexOptions, DirectoryIndexData,
-	ServerEventEmitter, ER, getNewTreePathFiles, isNewTreeGroup, NewTreePath, NewTreeItem, NewTreeGroup, NewTreePathOptions_Auth, StandardResponseHeaders
+	ServerEventEmitter, ER, getNewTreePathFiles, isNewTreeGroup, NewTreePath, NewTreeItem, NewTreeGroup, NewTreePathOptions_Auth, StandardResponseHeaders, serveFile
 } from "./server-types";
 
 import * as fs from 'fs';
@@ -29,7 +29,7 @@ import { AnonymousSubscription } from "rxjs/Subscription";
 import { send, formidable } from '../lib/bundled-lib';
 import { Stats } from "fs";
 import { last } from "rxjs/operator/last";
-import { NewTreeOptions, NewTreePathOptions_Backup, NewTreePathOptions_Index } from "./server-config";
+import { NewTreeOptions, NewTreePathOptions_Backup, NewTreePathOptions_Index, NewTreeOptionsObject } from "./server-config";
 
 const debug = DebugLogger("SER-API");
 __dirname = path.dirname(module.filename || process.execPath);
@@ -61,53 +61,50 @@ export function init(eventer: ServerEventEmitter) {
 }
 
 type apiListRouteState = [[string, string], string | any, StateObject]
-export function checkRouteAllowed(state: StateObject, result: PathResolverResult) {
-	type CC = (NewTreeGroup["$children"][0] | NewTreeOptions);
-	let lastAuth: NewTreePathOptions_Auth | undefined;
-	let findAuth = (f): f is NewTreePathOptions_Auth => f.$element === "auth";
-	result.ancestry.concat(result.item).forEach((e) => {
-		lastAuth = Array.isArray(e.$children) && (e.$children as CC[]).find(findAuth) || lastAuth;
-	});
-	// console.log(lastAuth, state.authAccountsKey);
-	return !lastAuth || lastAuth.authList.indexOf(state.authAccountsKey) !== -1;
-}
-export function getTreeOptions(state: StateObject, tree: ServerConfig["tree"]) {
-	let options: {
-		auth: NewTreePathOptions_Auth,
-		backups: NewTreePathOptions_Backup,
-		index: NewTreePathOptions_Index
-	} = {
-		auth: { $element: "auth", authError: "403", authList: null },
+// export function checkRouteAllowed(state: StateObject, result: PathResolverResult) {
+// 	return true;
+// 	type CC = (NewTreeGroup["$children"][0] | NewTreeOptions);
+// 	let lastAuth: NewTreePathOptions_Auth | undefined;
+// 	let findAuth = (f): f is NewTreePathOptions_Auth => f.$element === "auth";
+// 	result.ancestry.concat(result.item).forEach((e) => {
+// 		lastAuth = Array.isArray(e.$children) && (e.$children as CC[]).find(findAuth) || lastAuth;
+// 	});
+// 	// console.log(lastAuth, state.authAccountsKey);
+// 	return !lastAuth || lastAuth.authList.indexOf(state.authAccountsKey) !== -1;
+// }
+
+export function getTreeOptions(state: StateObject) {
+	let options: NewTreeOptionsObject = {
+		auth: { $element: "auth", authError: 403, authList: null },
 		backups: { $element: "backups", backupFolder: "", etagAge: 0, gzip: true },
 		index: { $element: "index", defaultType: "html", indexFile: [], indexExts: [] }
 	}
 	state.ancestry.forEach((e) => {
-		console.log(e.key);
+		console.log(e);
 		e.$children && e.$children.forEach((f) => {
 			if (f.$element === "auth" || f.$element === "backups" || f.$element === "index") {
 				Object.keys(f).forEach(k => {
-					if(f[k] === undefined) return;
+					if (f[k] === undefined) return;
 					options[f.$element][k] = f[k];
 				})
 			}
 		})
 	});
-
+	return options;
 }
 export function handleTiddlyServerRoute(state: StateObject) {
 
 	// const resolvePath = (settings.tree);
 	Observable.of(state).mergeMap((state: StateObject) => {
 		var result = resolvePath(state, settings.tree) as PathResolverResult;
-		state.ancestry = result.ancestry;
-		console.log(result.ancestry);
 		if (!result) return state.throw<never>(404);
+		state.ancestry = [...result.ancestry, result.item];
+		state.treeOptions = getTreeOptions(state);
 		//handle route authentication
-		if (!checkRouteAllowed(state, result)) {
-
-			return state.throw<never>(403);
-		}
-		if (isNewTreeGroup(result.item)) {
+		let { authList, authError } = state.treeOptions.auth;
+		if (authList && authList.indexOf(state.authAccountsKey) === -1) {
+			return state.throw<never>(authError);
+		} else if (isNewTreeGroup(result.item)) {
 			serveDirectoryIndex(result, state);
 			return Observable.empty<never>();
 		} else {
@@ -159,24 +156,73 @@ function handleFileError(err: NodeJS.ErrnoException) {
 function serveDirectoryIndex(result: PathResolverResult, state: StateObject) {
 	// const { state } = result;
 	const allow = state.allow;
+
 	// console.log(state.url);
 	if (!state.url.pathname.endsWith("/")) {
 		state.redirect(state.url.pathname + "/");
 	} else if (state.req.method === "GET") {
 		const isFolder = result.item.$element === "folder";
-		const options = {
-			upload: isFolder && (allow.upload),
-			mkdir: isFolder && (allow.mkdir),
-			mixFolders: settings.directoryIndex.mixFolders,
-			isLoggedIn: state.username ? (state.username + " (group " + state.authAccountsKey + ")") : false
-		};
-		getNewTreePathFiles(result, state)
-			.map(e => [e, options] as [typeof e, typeof options])
-			.concatMap(sendDirectoryIndex)
-			.subscribe(res => {
-				state.respond(200, "", { 'Content-Type': 'text/html', "Content-Encoding": 'utf-8' })
-					.buffer(Buffer.from(res, "utf8"));
+		Observable.of(state).concatMap(() => {
+			let { indexFile, indexExts, defaultType } = state.treeOptions.index;
+
+			if (isFolder && indexExts.length && indexFile.length) {
+				return obs_readdir()(result.fullfilepath).concatMap(([err, files]) => {
+					if (err) return state.log(2, 'Error calling readdir on folder "%s": %s', result.fullfilepath, err.message).throw(500);
+					let indexFiles: string[] = [];
+					indexFile.forEach(e => {
+						indexExts.forEach(f => {
+							if (f === "") indexFiles.push(e);
+							else indexFiles.push(e + "." + f);
+						});
+					});
+					let index = indexFiles.find((e) => files.indexOf(e) !== -1);
+					if (index) {
+						serveFile(state, index, result.fullfilepath);
+						return Observable.empty();
+					} else if (defaultType === 403 || defaultType === 404) {
+						return state.throw(defaultType);
+					}
+					return Observable.of(state);
+				});
+			} else if (result.item.$element === "group" && result.item.indexPath) {
+				let { indexPath } = result.item;
+				state.send({
+					root: null,
+					filepath: indexPath,
+					error: (err) => {
+						let error = new ER("error sending index", err.toString());
+						return state.log(2, error.message).throwError(500, error);
+					}
+				});
+				return Observable.empty();
+			}
+			else return Observable.of(state);
+		}).subscribe(() => {
+			const format = state.treeOptions.index.defaultType as "html" | "json";
+			const options = {
+				upload: isFolder && (allow.upload),
+				mkdir: isFolder && (allow.mkdir),
+				mixFolders: settings.directoryIndex.mixFolders,
+				isLoggedIn: state.username ? (state.username + " (group " + state.authAccountsKey + ")") : false,
+				format
+			};
+			let contentType = {
+				html: "text/html",
+				json: "application/json"
+			}
+			getNewTreePathFiles(result, state)
+				.map(e => [e, options] as [typeof e, DirectoryIndexOptions])
+				.concatMap(sendDirectoryIndex)
+				.subscribe(res => {
+					state.respond(200, "", { 'Content-Type': contentType[format], "Content-Encoding": 'utf-8' })
+						.buffer(Buffer.from(res, "utf8"));
+				});
+		})
+		if (isFolder && state.treeOptions.index.indexExts.length && state.treeOptions.index.indexFile.length) {
+
+			fs.readdir(result.fullfilepath, (err, files) => {
 			});
+		}
 	} else if (state.req.method === "POST") {
 		var form = new formidable.IncomingForm();
 		// console.log(state.url);
