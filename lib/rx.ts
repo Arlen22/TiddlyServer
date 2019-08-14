cb00ebcf283fe99094cb5d19a0a076d0e2426a7a
arlen22 <arlenbee@gmail.com>
2019-08-14 13:34:17 -0400

    Remove rxjs library

--- lib/rx.ts
+++ lib/rx.ts
@@ -1,6 +1,6 @@
 export { 
-    Observable, Subject, Subscription, BehaviorSubject, Subscriber, Observer, Scheduler, Operator 
+    // Observable, Subject, Subscription, BehaviorSubject, Subscriber, Observer, Scheduler, Operator 
 } from './Rx.min';
-import './rxjs-operators/routeCase.add';
+// import './rxjs-operators/routeCase.add';
 // import './rxjs-operators/chainMap';
 //export { Observable, Subject, Subscription, BehaviorSubject, Subscriber, Observer, Scheduler };
