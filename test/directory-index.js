926a86898fc8f59f33837ce52fdf7d3d879e7fc9
Arlen22 <arlenbee@gmail.com>
2020-01-18 21:58:50 -0500

    My first real regression test

---
+++ test/directory-index.js
@@ -0,0 +1,15 @@
+const fetch = require("node-fetch").default;
+if(!process.argv[2]) {
+  throw "Please specify the host as argv[2]";
+  // node test/directory-index.js http://localhost:8080
+}
+((host) => Promise.all([
+  fetch(host + "/test1/").then(res => { if (res.status !== 403) throw "1 not 403"; }),
+  fetch(host + "/test2/").then(res => { if (res.status !== 403) throw "2 not 403"; }),
+  fetch(host + "/test1").then(res => { if (!res.url.endsWith("/")) throw "1 not redirected"; }),
+  fetch(host + "/test2").then(res => { if (!res.url.endsWith("/")) throw "2 not redirected"; })
+]))(process.argv[2]).then(() => {
+  console.log("done");
+}).catch((err) => { 
+  console.log(err);
+});
