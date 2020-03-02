398f13021a9b1860dab81ef93654fe195263d136
Matt Young <dev@mttyng.com>
2020-03-01 21:54:02 -0800

    refactor: cleanup top-level server init file - added yargs as a dependency to assist in command-line parsing - cleaned up some logic for settingsFile parsing - minor cleanup

--- server.js
+++ server.js
@@ -1,86 +1,104 @@
 #!/usr/bin/env node
 
-
-
-//@ts-check
-require("./lib/source-map-support-lib");
-const fs = require('fs');
+require('./lib/source-map-support-lib')
+const fs = require('fs')
 const path = require('path');
 const { inspect } = require('util');
+const server = require("./src/server");
 
-var args = process.argv.slice(2);
-// console.log(process.version);
-const settingsFile = path.normalize(
-  (args[0] && args[0].indexOf('--') !== 0)
-    ? path.resolve(args.shift())
-    : path.join(__dirname, './settings.json')
-);
-const settingsPath = path.dirname(settingsFile);
+const SETTINGS_FILE = 'settings.json'
+const yargs = require('yargs')
+    .usage('./$0 - TiddlyServer')
+    .option('settings-path', {
+        describe: 'The location of the settings.json file',
+        demandOption: false,
+        type: 'string'
+    })
+    .option('stay-on-error', {
+        describe: 'Start a setInterval loop to keep the process\nfrom exiting.',
+        demandOption: false,
+        type: 'boolean'
+    })
+    .option('dry-run', {
+        describe: 'Do everything except call server.listen().\nUseful for checking settings.',
+        demandOption: false,
+        type: 'boolean'
+    })
 
+const { settingsPath, dryRun, stayOnError } = yargs.argv
+const settingsFile = settingsPath && typeof settingsPath === 'string'
+    ? fs.existsSync(`${settingsPath}/${SETTINGS_FILE}`)
+        ? settingsPath
+        : null
+    : fs.existsSync(`${SETTINGS_FILE}`)
+        ? path.join(__dirname, `./${SETTINGS_FILE}`)
+        : null
 
-/**
- *      DEVELOPERS PLEASE READ
- * 
- * This entire codebase is very dirty. 
- *    It will be cleaned up soon. 
- *              Enjoy!
- * 
- */
-
-/** @type {import("./src/server")} */
-// @ts-ignore
-const server = false ? require('./lib/compiled-lib') : require("./src/server");
-
-server.libsReady.then(() => {
-  // let [check1, checkErr1] = server.checkServerConfig(require(settingsFile));
-  // if (check1 !== true) { console.log(JSON.stringify(checkErr1, null, 2)); debugger; }
-  const { settings, settingshttps } = server.loadSettings(settingsFile, Object.keys(server.routes));
-  let [check, checkErr] = server.checkServerConfig(settings);
-  if (check !== true) { console.log(JSON.stringify(checkErr, null, 2)); debugger; }
-  // fs.writeFileSync("settings-temp.json", JSON.stringify(settings, null, 2));
-  server.eventer.emit("settings", settings);
-  let httpsSettingsFile = settingshttps ? path.resolve(settingsPath, settingshttps) : false;
-  server.initServer({
-    settings,
-    settingshttps: httpsSettingsFile && require(httpsSettingsFile).serverOptions,
-    preflighter: fs.existsSync(__dirname + "/preflighter.js")
-      ? require("./preflighter.js").preflighter
-      : undefined,
-    dryRun: args.indexOf("--dry-run") !== -1
-  });
-
-}).catch(e => {
-  console.log("uncaught error during server startup");
-  console.log(e);
-  console.log("exiting");
-  process.exitCode = 1;
-})
-
-const unhandled = (err) => {
-  process.exitCode = 1;
-  console.error(inspect(err));
-  console.error("caught process uncaughtException");
-  fs.appendFile(path.join(__dirname, 'uncaughtException.log'),
-    new Date().toISOString() + "\r\n" + inspect(err) + "\r\n\r\n", (err) => {
-      if (err) console.log('Could not write to uncaughtException.log');
-    });
+const closeServerUnlessStayOnError = (stayOnError) => {
   server.eventer.emit('serverClose', "all");
   //hold it open because all other listeners should close
-  if (args.indexOf("--stay-on-error") !== -1)
-    setInterval(function () { }, 1000);
+  if (stayOnError) setInterval(function () { }, 1000);
+}
+
+const logUncaughtExceptions = (err) => {
+  process.exitCode = 1;
+  console.error("[ERROR]: caught process uncaughtException", inspect(err));
+  fs.appendFile(path.join(__dirname, 'uncaughtException.log'),
+                new Date().toISOString() + "\r\n" + inspect(err) + "\r\n\r\n", (err) => {
+                  if (err) console.log('Could not write to uncaughtException.log');
+                });
 };
 
-// unhandled rejections with no reasons should be ignored
-process.on("unhandledRejection", (err, prom) => {
-  if (!err) return;
-  else unhandled(err);
-});
-process.on('uncaughtException', unhandled);
+const runServer = async (settingsFile) => {
+  const filePath = path.dirname(path.normalize(settingsFile))
+  await server.libsReady
 
+  const { settings, settingshttps } = server.loadSettings(
+    settingsFile, Object.keys(server.routes)
+  );
+  const [check, checkErr] = server.checkServerConfig(settings);
+  if (!check) {
+    console.log(JSON.stringify(checkErr, null, 2));
+    debugger;
+  }
+  server.eventer.emit("settings", settings);
 
-process.on('beforeExit', () => {
-  if (process.exitCode) return;
-  // console.log('The process was about to close with exitCode 0 -- restarting server');
-  // server.initServer({ env: "node", settings });
-})
+  let httpsSettingsFile = settingshttps
+      ? path.resolve(filePath, settingshttps)
+      : false;
 
+  try {
+    server.initServer({
+      settings,
+      settingshttps: httpsSettingsFile && require(httpsSettingsFile).serverOptions,
+      preflighter: fs.existsSync(__dirname + "/preflighter.js")
+        ? require("./preflighter.js").preflighter
+        : undefined,
+      dryRun
+    });
+  } catch (e) {
+    console.error("[ERROR]: Uncaught error during server startup:", e);
+    process.exit(1);
+  }
+
+  // Unhandled rejections with no reasons should be ignored
+  process.on("unhandledRejection", (err, _prom) => {
+    if (!err) return;
+    logUncaughtExceptions(err);
+    closeServerUnlessStayOnError(stayOnError);
+  });
+  process.on('uncaughtException', logUncaughtExceptions);
+  process.on('beforeExit', () => {
+    if (process.exitCode) {
+      console.log('Server exited with errors');
+      return
+    }
+  })
+}
+
+if (settingsFile) {
+  runServer(settingsFile);
+} else {
+  console.error('\n[ERROR]: No `settings.json` file found in path.\nConsider passing its location via --settings-path\n')
+  process.exit(1)
+}
