b509a1536bd5b49f2b05cf1796678aec5f5add03
Arlen22 <arlenbee@gmail.com>
2020-03-06 21:13:24 -0500

    Merge changes to server.ts
    Merge PR https://github.com/Arlen22/TiddlyServer/pull/83 from @someguynamedmatt

--- server.ts
+++ server.ts
@@ -1,36 +1,52 @@
 #!/usr/bin/env node
+
 require("source-map-support/register");
-//@ts-check
-const fs = require("fs");
-const path = require("path");
-const { inspect } = require("util");
-const { SettingsReader } = require("./src/settingsReader");
-
-var args = process.argv.slice(2);
-// console.log(process.version);
-
-const settingsFile = path.normalize(
-  args[0] && args[0].indexOf("--") !== 0
-    ? path.resolve(args.shift())
-    : path.join(
-        __dirname,
-        //if we're in the TiddlyServer/build directory the default is in the root
-        __dirname.endsWith("/build") ? ".." : "",
-        "settings.json"
-      )
-);
-const settingsPath = path.dirname(settingsFile);
-
-/**
- *      DEVELOPERS PLEASE READ
- *
- * This entire codebase is very dirty.
- *    It will be cleaned up soon.
- *              Enjoy!
- *
- */
-
+import fs = require("fs");
+import path = require("path");
+import { inspect } from "util";
 import server = require("./src/server");
+import yargs = require("yargs");
+
+const SETTINGS_FILE = "settings.json";
+const argv = yargs
+  .usage("./$0 - TiddlyServer")
+  .option("config", {
+    describe: "Path to the server config file",
+    demandOption: false,
+    type: "string",
+  })
+  .option("stay-on-error", {
+    describe: "Start a setInterval loop to keep the process\nfrom exiting.",
+    demandOption: false,
+    type: "boolean",
+    default: false,
+  })
+  .option("dry-run", {
+    describe:
+      "Do everything except call server.listen().\nUseful for checking settings.",
+    demandOption: false,
+    default: false,
+    type: "boolean",
+  }).argv;
+
+const {
+  config: userSettings,
+  "dry-run": dryRun,
+  "stay-on-error": stayOnError,
+} = argv;
+
+const settingsFile = userSettings
+  ? path.resolve(userSettings)
+  : path.join(
+      __dirname,
+      //if we're in the build directory the default one level up
+      __dirname.endsWith("/build") ? ".." : "",
+      "settings.json"
+    );
+
+import { SettingsReader } from "./src/settingsReader";
+
+// import server = require("./src/server");
 const settingsReader = SettingsReader.getInstance();
 declare const __non_webpack_require__: NodeRequire | undefined;
 const nodeRequire =
@@ -38,26 +54,59 @@ const nodeRequire =
     ? __non_webpack_require__
     : require;
 
-server.libsReady
-  .then(() => {
-    // let [check1, checkErr1] = server.checkServerConfig(require(settingsFile));
-    // if (check1 !== true) { console.log(JSON.stringify(checkErr1, null, 2)); debugger; }
-    const { settings, settingshttps } = server.loadSettings(
-      settingsFile,
-      Object.keys(server.routes)
+const logAndCloseServer = (err: any) => {
+  server.eventer.emit("serverClose", "all");
+  //hold it open because all other listeners should close
+  if (stayOnError) setInterval(function() {}, 1000);
+  process.exitCode = 1;
+  console.error("[ERROR]: caught process uncaughtException", inspect(err));
+  try {
+    fs.appendFileSync(
+      path.join(__dirname, "uncaughtException.log"),
+      new Date().toISOString() + "\r\n" + inspect(err) + "\r\n\r\n"
     );
-    // Store the settings object into the reader to allow easier access elsewhere
-    settingsReader.storeServerSettings(settings);
-    let [check, checkErr] = server.checkServerConfig(settings);
-    if (check !== true) {
-      console.log(JSON.stringify(checkErr, null, 2));
-      debugger;
-    }
-    // fs.writeFileSync("settings-temp.json", JSON.stringify(settings, null, 2));
-    server.eventer.emit("settings", settings);
-    let httpsSettingsFile = settingshttps
-      ? path.resolve(settingsPath, settingshttps)
-      : false;
+  } catch (e) {
+    console.log("Could not write to uncaughtException.log");
+  }
+};
+
+// Unhandled rejections with no reasons should be ignored
+process.on("unhandledRejection", (err, _prom) => {
+  if (!err) return;
+  logAndCloseServer(err);
+});
+process.on("uncaughtException", err => {
+  logAndCloseServer(err);
+});
+process.on("beforeExit", () => {
+  if (process.exitCode) {
+    console.log("Server exited with errors " + process.exitCode);
+    return;
+  }
+});
+
+async function runServer() {
+  const settingsDir = path.dirname(settingsFile);
+  await server.libsReady;
+
+  const { settings, settingshttps } = server.loadSettings(
+    settingsFile,
+    Object.keys(server.routes)
+  );
+  settingsReader.storeServerSettings(settings);
+  const [check, checkErr] = server.checkServerConfig(settings);
+
+  if (!check) {
+    console.log(JSON.stringify(checkErr, null, 2));
+    debugger;
+  }
+  server.eventer.emit("settings", settings);
+
+  let httpsSettingsFile = settingshttps
+    ? path.resolve(settingsDir, settingshttps)
+    : false;
+
+  try {
     server.initServer({
       settings,
       settingshttps:
@@ -65,41 +114,18 @@ server.libsReady
       preflighter: fs.existsSync(__dirname + "/preflighter.js")
         ? nodeRequire("./preflighter.js").preflighter
         : undefined,
-      dryRun: args.indexOf("--dry-run") !== -1,
+      dryRun,
     });
-  })
-  .catch(e => {
-    console.log("uncaught error during server startup");
-    console.log(e);
-    console.log("exiting");
-    process.exitCode = 1;
-  });
+  } catch (e) {
+    console.error("[ERROR]: Uncaught error during server startup:", e);
+    process.exit(1);
+  }
+}
 
-const unhandled = err => {
-  process.exitCode = 1;
-  console.error(inspect(err));
-  console.error("caught process uncaughtException");
-  fs.appendFile(
-    path.join(__dirname, "uncaughtException.log"),
-    new Date().toISOString() + "\r\n" + inspect(err) + "\r\n\r\n",
-    err => {
-      if (err) console.log("Could not write to uncaughtException.log");
-    }
+if (fs.existsSync(settingsFile)) {
+  runServer();
+} else {
+  logAndCloseServer(
+    "[ERROR]: server config file could not be found.\nConsider passing its location via --config\n"
   );
-  server.eventer.emit("serverClose", "all");
-  //hold it open because all other listeners should close
-  if (args.indexOf("--stay-on-error") !== -1) setInterval(function() {}, 1000);
-};
-
-// unhandled rejections with no reasons should be ignored
-process.on("unhandledRejection", (err, prom) => {
-  if (!err) return;
-  else unhandled(err);
-});
-process.on("uncaughtException", unhandled);
-
-process.on("beforeExit", () => {
-  if (process.exitCode) return;
-  // console.log('The process was about to close with exitCode 0 -- restarting server');
-  // server.initServer({ env: "node", settings });
-});
+}
