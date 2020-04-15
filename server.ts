09b79a20ea64807591c26a1e2b1226e1307877cd
Matt Young <dev@mttyng.com>
2020-04-15 06:23:29 -0700

    server.ts: preflighter always gets loaded

--- server.ts
+++ server.ts
@@ -1,128 +1,113 @@
 #!/usr/bin/env node
 
-require("source-map-support/register");
+require('source-map-support/register')
 import * as fs from 'fs'
 import * as path from 'path'
-import { inspect } from "util";
-import * as server from "./src/server"
-import * as yargs from "yargs"
+import { inspect } from 'util'
+import * as server from './src/server'
+import * as yargs from 'yargs'
+import preflighter from './preflighter'
 
-const SETTINGS_FILE = "settings.json";
+const SETTINGS_FILE = 'settings.json'
 const argv = yargs
-  .usage("./$0 - TiddlyServer")
-  .option("config", {
-    describe: "Path to the server config file",
+  .usage('./$0 - TiddlyServer')
+  .option('config', {
+    describe: 'Path to the server config file',
     demandOption: false,
-    type: "string",
+    type: 'string',
   })
-  .option("stay-on-error", {
-    describe: "Start a setInterval loop to keep the process\nfrom exiting.",
+  .option('stay-on-error', {
+    describe: 'Start a setInterval loop to keep the process\nfrom exiting.',
     demandOption: false,
-    type: "boolean",
+    type: 'boolean',
     default: false,
   })
-  .option("dry-run", {
-    describe:
-      "Do everything except call server.listen().\nUseful for checking settings.",
+  .option('dry-run', {
+    describe: 'Do everything except call server.listen().\nUseful for checking settings.',
     demandOption: false,
     default: false,
-    type: "boolean",
-  }).argv;
+    type: 'boolean',
+  }).argv
 
-const {
-  config: userSettings,
-  "dry-run": dryRun,
-  "stay-on-error": stayOnError,
-} = argv;
+const { 'config': userSettings, 'dry-run': dryRun, 'stay-on-error': stayOnError } = argv
 
 const settingsFile = userSettings
   ? path.resolve(userSettings)
   : path.join(
       __dirname,
       //if we're in the build directory the default one level up
-      __dirname.endsWith("/build") ? ".." : "",
-      "settings.json"
-    );
+      __dirname.endsWith('/build') ? '..' : '',
+      'settings.json'
+    )
 
-
-declare const __non_webpack_require__: NodeRequire | undefined;
+declare const __non_webpack_require__: NodeRequire | undefined
 const nodeRequire =
-  typeof __non_webpack_require__ !== "undefined"
-    ? __non_webpack_require__
-    : require;
+  typeof __non_webpack_require__ !== 'undefined' ? __non_webpack_require__ : require
 
 const logAndCloseServer = (err: any) => {
-  server.eventer.emit("serverClose", "all");
+  server.eventer.emit('serverClose', 'all')
   //hold it open because all other listeners should close
-  if (stayOnError) setInterval(function() {}, 1000);
-  process.exitCode = 1;
-  console.error("[ERROR]: caught process uncaughtException", inspect(err));
+  if (stayOnError) setInterval(function() {}, 1000)
+  process.exitCode = 1
+  console.error('[ERROR]: caught process uncaughtException', inspect(err))
   try {
     fs.appendFileSync(
-      path.join(__dirname, "uncaughtException.log"),
-      new Date().toISOString() + "\r\n" + inspect(err) + "\r\n\r\n"
-    );
+      path.join(__dirname, 'uncaughtException.log'),
+      new Date().toISOString() + '\r\n' + inspect(err) + '\r\n\r\n'
+    )
   } catch (e) {
-    console.log("Could not write to uncaughtException.log");
+    console.log('Could not write to uncaughtException.log')
   }
-};
+}
 
 // Unhandled rejections with no reasons should be ignored
-process.on("unhandledRejection", (err, _prom) => {
-  if (!err) return;
-  logAndCloseServer(err);
-});
-process.on("uncaughtException", err => {
-  logAndCloseServer(err);
-});
-process.on("beforeExit", () => {
+process.on('unhandledRejection', (err, _prom) => {
+  if (!err) return
+  logAndCloseServer(err)
+})
+process.on('uncaughtException', err => {
+  logAndCloseServer(err)
+})
+process.on('beforeExit', () => {
   if (process.exitCode) {
-    console.log("Server exited with errors " + process.exitCode);
-    return;
+    console.log('Server exited with errors ' + process.exitCode)
+    return
   }
-});
+})
 
 async function runServer() {
-  const settingsDir = path.dirname(settingsFile);
-  await server.libsReady;
+  const settingsDir = path.dirname(settingsFile)
+  await server.libsReady
 
-  const { settings, settingshttps } = server.loadSettings(
-    settingsFile,
-    Object.keys(server.routes)
-  );
-  
-  const [check, checkErr] = server.checkServerConfig(settings);
+  const { settings, settingshttps } = server.loadSettings(settingsFile, Object.keys(server.routes))
+
+  const [check, checkErr] = server.checkServerConfig(settings)
 
   if (!check) {
-    console.log(JSON.stringify(checkErr, null, 2));
-    debugger;
+    console.log(JSON.stringify(checkErr, null, 2))
+    debugger
   }
-  server.eventer.emit("settings", settings);
+  server.eventer.emit('settings', settings)
 
-  let httpsSettingsFile = settingshttps
-    ? path.resolve(settingsDir, settingshttps)
-    : false;
+  let httpsSettingsFile = settingshttps ? path.resolve(settingsDir, settingshttps) : false
 
   try {
     server.initServer({
       settings,
-      settingshttps:
-        httpsSettingsFile && nodeRequire(httpsSettingsFile).serverOptions,
-      preflighter: fs.existsSync(__dirname + "/preflighter.js")
-        ? nodeRequire("./preflighter.js").preflighter
-        : undefined,
+      settingshttps: httpsSettingsFile && nodeRequire(httpsSettingsFile).serverOptions,
+      preflighter,
       dryRun,
-    });
+    })
   } catch (e) {
-    console.error("[ERROR]: Uncaught error during server startup:", e);
-    process.exit(1);
+    console.error('[ERROR]: Uncaught error during server startup:', e)
+    process.exit(1)
   }
 }
 
 if (fs.existsSync(settingsFile)) {
-  runServer();
+  runServer()
 } else {
   logAndCloseServer(
-    "[ERROR]: server config file could not be found.\nConsider passing its location via --config\n"
-  );
+    '[ERROR]: server config file could not be found.\nConsider passing its location via --config\n'
+  )
 }
