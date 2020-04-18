86d96fc4b8a5a32985d2510cfadd2fcc661c5905
Matt Young <dev@mttyng.com>
2020-04-18 11:27:09 -0700

    moved build-schema into /schemas and converted to TS

--- schemas/build-schema.ts
+++ schemas/build-schema.ts
@@ -8,22 +8,22 @@ const { writeFileSync } = require('fs')
 const stringify = require('json-stable-stringify')
 const config = {
   ...DEFAULT_CONFIG,
-  path: 'src/server-config.ts',
-  tsconfig: './tsconfig.json',
+  path: '../src/server-config.ts',
+  tsconfig: 'tsconfig.json',
   type: 'ServerConfigSchema',
   sortProps: true,
 }
 
 try {
   let root = createGenerator(config).createSchema('ServerConfigSchema')
-  root['$id'] = './schemas/settings-2-1.schema.json'
+  root['$id'] = './settings-2-1.schema.json'
   root['definitions']['ServerConfigSchema']['properties']['tree'] = {
-    $ref: './schemas/settings-2-1-tree.schema.json',
+    $ref: './settings-2-1-tree.schema.json',
   }
   let options = createGenerator(config).createSchema('OptionsArraySchema')
-  options['$id'] = './schemas/settings-2-1-tree-options.schema.json'
-  writeFileSync('./schemas/settings-2-1.schema.json', stringify(root, { space: 2 }))
-  writeFileSync('./schemas/settings-2-1-tree-options.schema.json', stringify(options, { space: 2 }))
+  options['$id'] = './settings-2-1-tree-options.schema.json'
+  writeFileSync('./settings-2-1.schema.json', stringify(root, { space: 2 }))
+  writeFileSync('./settings-2-1-tree-options.schema.json', stringify(options, { space: 2 }))
 } catch (error) {
   if (error instanceof BaseError) {
     process.stderr.write(formatError(error))
